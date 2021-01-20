d6c7f00da06c9d06bcf76ed44d6911076b357e7d
TeoWeiShan <S10205156@connect.np.edu.sg>
2021-01-20 19:32:38 +0800

    Add project files.

---
+++ PRG2_T04_Team5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRG2_T04_Team5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
